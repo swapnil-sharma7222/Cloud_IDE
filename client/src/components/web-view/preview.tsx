f1db0bf8f5662c4c8a03af5fb1241fee515a3b9e
swanil-sharma7222 <2021uee1371@mnit.ac.in>
2025-03-24 16:10:35 +0530

    initial commit

--- client/src/components/web-view/preview.tsx
+++ client/src/components/web-view/preview.tsx
@@ -1,32 +1,3 @@
-// import { Box } from "@mui/material";
-// import { useContext } from "react";
-// import { WebViewContext } from "./context";
-
-// const Preview: React.FC= ()=> {
-//   const context= useContext(WebViewContext)
-
-//   if(context){
-//     throw new Error("The WebView context is not provided in the preview");
-//   }
-
-//   console.log(context);
-//   // const url= "http://localhost:5173"
-//   const { url }= context;
-
-//   return (
-//     <Box sx={{ flexGrow: 1, overflow: "scroll" }}>
-//     <iframe
-//       src={url}
-//       width="100%"
-//       height="100%"
-//       style={{ border: "none" }}
-//       title="Webview"
-//     />
-//   </Box>
-//   )
-// }
-// export default Preview
-
 import { Box } from "@mui/material";
 import { useContext } from "react";
 import { WebViewContext } from "./context";
